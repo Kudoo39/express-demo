de525b00b08b269eda31f7b6f207351d926b93f6
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-07-04 21:14:05 +0300

    update HTTP POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -14,8 +16,20 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+//HTTP POST Request
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
-//api/posts/:year
+
+
+
+//api
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with given ID was not found!!');//404
