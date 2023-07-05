da8781dc7da81368a6ea91480147f4a0d503c865
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-07-05 03:04:10 +0300

    joi update

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ app.get('/api/courses', (req, res) => {
 
 //INPUT VALIDADION
 app.post('api/courses', (req, res) => {
-    if(!req.body.name || req.body.name.length < 3) {
-        res.status(400).send('Name is required and should be minimum 3 characters');
+    const scheme = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error) {
+        res.status(400).send(result.error.);
         return;
     }
 })
